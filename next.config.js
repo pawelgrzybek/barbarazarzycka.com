6ec3d8f654b760a0abb816736edadb00bddf10c8
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2024-03-01 15:02:28 +0000

    update tagline

--- next.config.js
+++ next.config.js
@@ -2,8 +2,7 @@
 const nextConfig = {
   env: {
     title: "Barbara Zarzycka",
-    description:
-      "Marketing Executive Account Developer at the Institute of Physics Publishing",
+    description: "Marketing Executive Account Developer",
   },
   images: {
     domains: ["cdn.sanity.io"],
